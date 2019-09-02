010433a205e7fa004d699f07f7f7fb5671c68d60
ds10 <ds10@localhost>
2019-09-02 06:46:53 +0000

    Changed version to 4.0.9.99 RC3.

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -161,7 +161,7 @@ public class TVBrowser {
   private static final boolean mIsStable = false;
   private static final int mMajorVersion = 4;
   private static final int mMinorVersion = 9;
-  private static final int mSubMinorVersion = 98;
+  private static final int mSubMinorVersion = 99;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
@@ -175,6 +175,7 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
+      "4.0.9.98 RC3",
       "4.0.9.98 RC2",
 		  "4.0.9.97 RC1",
 		  "4.0.9.96 Beta2",
