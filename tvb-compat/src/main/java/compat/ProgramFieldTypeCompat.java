578fd63274b741b1d0e45a2e192226f72adece8c
ds10 <ds10@localhost>
2020-01-20 14:32:56 +0000

    Added compat class for devplugin.ProgramFieldType

---
+++ tvb-compat/src/main/java/compat/ProgramFieldTypeCompat.java
@@ -0,0 +1,53 @@
+/*
+ * TV-Browser Compat
+ * Copyright (C) 2017 TV-Browser team (dev@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * SVN information:
+ *     $Date: 2014-06-17 15:59:09 +0200 (Di, 17 Jun 2014) $
+ *   $Author: ds10 $
+ * $Revision: 8152 $
+ */
+package compat;
+
+/**
+ * A compatibility class for TV-Browser devplugin.ProgramFieldType class.
+ * 
+ * @author René Mach
+ * @since 0.6
+ */
+public class ProgramFieldTypeCompat {
+  /**
+   * unknown field format, should not occur
+   */
+   public static final int FORMAT_UNKNOWN = 1;
+   /**
+    * program field format for binary fields (like pictures)
+    */
+   public static final int FORMAT_BINARY = 2;
+   /**
+    * program field format for strings
+    */
+   public static final int FORMAT_TEXT = 3;
+   /**
+    * program field format for numbers
+    */
+   public static final int FORMAT_INT = 4;
+   /**
+    * program field format for times (in numbers after midnight)
+    */
+   public static final int FORMAT_TIME = 5;
+}
