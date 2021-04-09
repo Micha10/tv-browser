10921247b6621e000f1f99b66e9bb650edc3e1d4
ds10 <ds10@localhost>
2021-04-09 21:40:52 +0000

    Find more long forms, don't ungender Scottish McInnen names.

--- tvbrowser-plugins/plugin-degender/src/main/java/degenderplugin/DegenderPlugin.java
+++ tvbrowser-plugins/plugin-degender/src/main/java/degenderplugin/DegenderPlugin.java
@@ -112,15 +112,37 @@ public class DegenderPlugin extends Plugin {
       CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_ARD-Radio_de_SWR4.RP");
       CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_digital_de_einsextra");
       CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_digital_de_einstfestival");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_austria_at_orf2");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_austria_at_orf2europe");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_austria_at_orf1");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_austria_at_orf3");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRB");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRK");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_ch_drs1");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_ch_drs2");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_ch_drs3");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_ch_drs4");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_ch_drs.musikwelle");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_others_ch_sfdrs1");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_others_ch_sfinfo");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_others_ch_sfdrs2");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRN");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFROE");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRS");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRST");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRT");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRV");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_radio_at_ORFRW");
+      CHANNELS_DEFAULT.add("tvbrowserdataservice.TvBrowserDataService_austria_at_orfsportplus");
   };
   
   private HashMap<String, String> mSingularReplacement;
   private HashMap<String, String> mPluralReplacement;
   private HashSet<Channel> mChannelSet;
   
-  private static final Pattern GENDERED = Pattern.compile("(\\b(?i)(die\\s){0,1}(?-i)\\b(\\w+?)(?:\\s*[\\*\\:](?i:i)|I)n(nen){0,1})", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS); 
-  private static final Pattern GENDERED_LONG = Pattern.compile("(\\b(\\w+?)innen\\b\\s+(?:und|oder)\\s+\\b(\\w+?)\\b)|(\\b(\\w+?)\\b\\s+(?:und|oder)\\s+\\b(\\w+?innen)\\b)", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS);
-  private static final Version VERSION = new Version(0,6,false);
+  private static final Pattern GENDERED = Pattern.compile("(\\b(?i)(die\\s){0,1}(?-i)\\b(?!Mc)(\\w+?)(?:\\s*[\\*\\:_](?i:i)|I)n(nen){0,1})", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS); 
+  private static final Pattern GENDERED_LONG = Pattern.compile("(\\b([\\w\\-]+?)innen\\b\\s+(?:und|oder)\\s+\\-{0,1}\\b(\\w+?)\\b)|(\\b([\\w\\-]+?)\\b\\s+(?:und|oder)\\s+\\-{0,1}\\b(\\w+?)innen\\b)", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS);
+  private static final Version VERSION = new Version(0,8,false);
   
   private boolean mRemoveLongForm = false;
   
@@ -151,7 +173,7 @@ public class DegenderPlugin extends Plugin {
   
   @Override
   public PluginInfo getInfo() {
-    return new PluginInfo(DegenderPlugin.class, "DeGender", "Entfernt Binnen-I, Gender-* und Gender-: aus Titel, Episodentitel, Serientitel, Kurzbeschreibung, Beschreibung, Bildbeschreibung und weitere Informationen.", "René Mach");
+    return new PluginInfo(DegenderPlugin.class, "DeGender", "Entfernt Binnen-I, Gender-*, Gender-: und Gender-_ aus Titel, Episodentitel, Serientitel, Kurzbeschreibung, Beschreibung, Bildbeschreibung und weitere Informationen.", "René Mach");
   }
   
   @Override
@@ -192,101 +214,106 @@ public class DegenderPlugin extends Plugin {
   
   private String deGender(String text) {
     if(text != null) {
-      
       Matcher m = GENDERED.matcher(text);
-      
-      int pos = 0;
       StringBuilder result = new StringBuilder();
       
-      while(m.find(pos)) {
-        /*for(int i = 1; i <= m.groupCount(); i++) {
-          System.out.println(i+": " + m.group(i));
-        }*/
+      do {
+        int pos = 0;
         
-        boolean wrongArticle = (m.group(2) != null && !m.group(2).trim().isEmpty());
-        String replace = m.group(3);
-        String test = replace.toLowerCase();
-        boolean singular = (m.group(4) == null || m.group(4).trim().isEmpty());
-        
-        if(!singular) {
-          Set<String> keys = mPluralReplacement.keySet();
-          boolean found = false;
-          for(String key : keys) {
-            if(test.endsWith(key.toLowerCase())) {
-              found = true;
-              String replacement = mPluralReplacement.get(key);
-              
-              if(replace.charAt(replace.length()-key.length()) != key.charAt(0)) {
-                replacement = replacement.toLowerCase();
+        while(m.find(pos)) {
+          /*for(int i = 1; i <= m.groupCount(); i++) {
+            System.out.println(i+": " + m.group(i));
+          }
+          System.out.println();*/
+          
+          boolean wrongArticle = (m.group(2) != null && !m.group(2).trim().isEmpty());
+          String replace = m.group(3);
+          String test = replace.toLowerCase();
+          boolean singular = (m.group(4) == null || m.group(4).trim().isEmpty());
+          
+          if(!singular) {
+            Set<String> keys = mPluralReplacement.keySet();
+            boolean found = false;
+            for(String key : keys) {
+              if(test.endsWith(key.toLowerCase())) {
+                found = true;
+                String replacement = mPluralReplacement.get(key);
+                
+                if(replace.charAt(replace.length()-key.length()) != key.charAt(0)) {
+                  replacement = replacement.toLowerCase();
+                }
+                
+                replace = replace.substring(0,replace.length()-key.length()) + replacement;
+                
+                break;
               }
-              
-              replace = replace.substring(0,replace.length()-key.length()) + replacement;
-              
-              break;
+            }
+            
+            if(!found && !test.endsWith("er")) {
+              replace += "en";
+            }
+          }
+          else {
+            Set<String> keys = mSingularReplacement.keySet();
+            for(String key : keys) {
+              if(test.endsWith(key.toLowerCase())) {
+                String replacement = mSingularReplacement.get(key);
+                
+                if(replace.charAt(replace.length()-key.length()) != key.charAt(0)) {
+                  replacement = replacement.toLowerCase();
+                }
+                
+                replace = replace.substring(0,replace.length()-key.length()) + replacement;
+                
+                break;
+              }
+            }
+            /*if(test.endsWith("ärzt")) {
+              replace = replace.replace("Ärzt","Arzt").replace("ärzt","arzt");
+            }
+            else if(test.endsWith("beamt")) {
+              replace = replace.replace("Beamt","Beamter").replace("beamt","beamter");
+            }
+            else if(test.endsWith("zeug")) {
+              replace += "e";
+            }
+            else if(test.endsWith("bäuer")) {
+              replace = replace.replace("Bäuer","Bauer").replace("bäuer","bäuer");
+            }*/
+            
+            if(wrongArticle) {
+              if(m.group(2).startsWith("D")) {
+                replace = "Der "+replace;
+              }
+              else {
+                replace = "der "+replace;
+              }
             }
           }
           
-          if(!found && !test.endsWith("er")) {
-            replace += "en";
-          }
-        }
-        else {
-          Set<String> keys = mSingularReplacement.keySet();
-          for(String key : keys) {
-            if(test.endsWith(key.toLowerCase())) {
-              String replacement = mSingularReplacement.get(key);
-              
-              if(replace.charAt(replace.length()-key.length()) != key.charAt(0)) {
-                replacement = replacement.toLowerCase();
-              }
-              
-              replace = replace.substring(0,replace.length()-key.length()) + replacement;
-              
-              break;
-            }
-          }
-          /*if(test.endsWith("ärzt")) {
-            replace = replace.replace("Ärzt","Arzt").replace("ärzt","arzt");
-          }
-          else if(test.endsWith("beamt")) {
-            replace = replace.replace("Beamt","Beamter").replace("beamt","beamter");
-          }
-          else if(test.endsWith("zeug")) {
-            replace += "e";
-          }
-          else if(test.endsWith("bäuer")) {
-            replace = replace.replace("Bäuer","Bauer").replace("bäuer","bäuer");
-          }*/
-          
-          if(wrongArticle) {
-            if(m.group(2).startsWith("D")) {
-              replace = "Der "+replace;
-            }
-            else {
-              replace = "der "+replace;
-            }
-          }
+          result.append(text.substring(pos,m.start(1))).append(replace);
+          pos = m.end();
         }
         
-        result.append(text.substring(pos,m.start(1))).append(replace);
-        pos = m.end();
-      }
-      
-      if(pos < text.length()) {
-        result.append(text.substring(pos,text.length()));
-      }
+        if(pos < text.length()) {
+          result.append(text.substring(pos,text.length()));
+        }
+        
+        text = result.toString();
+        result.setLength(0);
+        
+        m = GENDERED.matcher(text);
+      }while(m.find());
       
       if(mRemoveLongForm) {
-        pos = 0;
-        text = result.toString();
+        int pos = 0;
         
         m = GENDERED_LONG.matcher(text);
-        result.setLength(0);
-      
+        
         while(m.find(pos)) {
           /*for(int i = 1; i <= m.groupCount(); i++) {
           System.out.println(i+": " + m.group(i));
-        }*/
+        }System.out.println();*/
           String needle = null;
           String replace = null;
           String male = null;
@@ -298,6 +325,12 @@ public class DegenderPlugin extends Plugin {
             female = m.group(2).toLowerCase().replace("ä", "a").replace("ö", "o").replace("ü", "u");
             replace = m.group(3);
             male = replace.toLowerCase().replace("ä", "a").replace("ö", "o").replace("ü", "u");
+            
+            if(female.contains("-")) {
+              female = female.substring(female.lastIndexOf("-")+1);
+              replace = m.group(2).substring(0,m.group(2).lastIndexOf("-")+1)+replace;
+            }
+            
             index = m.start(1);
           }
           else if(m.group(4) != null && m.group(5) != null && m.group(6) != null) {
@@ -305,6 +338,11 @@ public class DegenderPlugin extends Plugin {
             female = m.group(6).toLowerCase().replace("ä", "a").replace("ö", "o").replace("ü", "u");
             replace = m.group(5);
             male = replace.toLowerCase().replace("ä", "a").replace("ö", "o").replace("ü", "u");
+            
+            if(male.contains("-")) {
+              male = male.substring(male.lastIndexOf("-")+1);
+            }
+            
             index = m.start(4);
           }
           
