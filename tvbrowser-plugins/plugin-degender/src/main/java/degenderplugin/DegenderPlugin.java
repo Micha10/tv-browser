d56a6d2776943e725530b65d1d8a76d05b56d0b1
ds10 <ds10@localhost>
2021-05-11 10:03:04 +0000

    Better Partizip recognition.

--- tvbrowser-plugins/plugin-degender/src/main/java/degenderplugin/DegenderPlugin.java
+++ tvbrowser-plugins/plugin-degender/src/main/java/degenderplugin/DegenderPlugin.java
@@ -45,8 +45,8 @@ public class DegenderPlugin extends Plugin {
   
   private static final Pattern GENDERED = Pattern.compile("(\\b(?i)(die\\s){0,1}(?-i)\\b(?!Mc)(\\w+?)(?:\\s*[\\*\\:_](?i:i)|I)n(nen){0,1})", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS); 
   private static final Pattern GENDERED_LONG = Pattern.compile("(\\b([\\w\\-]+?)innen\\b\\s+(?:und|oder)\\s+\\-{0,1}\\b(\\w+?)\\b)|(\\b([\\w\\-]+?)\\b\\s+(?:und|oder)\\s+\\-{0,1}\\b(\\w+?)innen\\b)", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS);
-  private static final Pattern GENDERED_PARTIZIP = Pattern.compile("(\\b(?i)(?:(die|den|bei)\\s){0,1}(?-i)\\b((\\p{Upper}\\w+)ende(n|r){0,1}(\\w*)\\b))", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS);
-  private static final Version VERSION = new Version(0,12,2,true);
+  private static final Pattern GENDERED_PARTIZIP = Pattern.compile("(\\b(?i)(?:(die|bei|mit|\\w+en)\\s){0,1}(?-i)(?:\\b\\w+\\b\\s){0,1}\\b((\\p{Upper}\\w+)ende(n|r){0,1}(\\w*)\\b))", Pattern.DOTALL | Pattern.UNICODE_CHARACTER_CLASS);
+  private static final Version VERSION = new Version(0,13,0,true);
   
   private boolean mRemoveLongForm = false;
   private boolean mReplacePartizip = false;
@@ -292,7 +292,7 @@ public class DegenderPlugin extends Plugin {
         */
         String replace = m.group(1);
         
-        if(m.group(5) != null || m.group(2) == null) {
+      /*  if(m.group(5) != null || m.group(2) == null) {
           if(m.group(4).toLowerCase().endsWith("studier") || m.group(4).toLowerCase().endsWith("dozier")) {
             replace = m.group(4).substring(0,m.group(4).length()-3)+"enten";
             
@@ -336,13 +336,59 @@ public class DegenderPlugin extends Plugin {
            // System.out.println(m.group(1) + " " + m.group(6) + " " + replace);
             mCountPartizip++;
           }
+        }*/
+        
+        if(m.group(4).toLowerCase().endsWith("studier") || m.group(4).toLowerCase().endsWith("dozier")) {
+          replace = m.group(1).substring(0,m.group(1).length()-m.group(3).length()+1) + m.group(4).substring(1,m.group(4).length()-3)+"ent";
+          
+          if(m.group(2) == null || (m.group(5) != null && m.group(5).equals("n"))) {
+            replace += "en";
+          }
+        }
+        else if(m.group(4).toLowerCase().endsWith("kunstschaff")) {
+          replace = m.group(1).substring(0,m.group(1).length()-m.group(3).length()+1) + m.group(4).substring(1,m.group(4).length()-10)+"ünstler";
+        }
+        else if(m.group(4).toLowerCase().endsWith("forsch") || m.group(4).toLowerCase().endsWith("lehr") 
+            || m.group(4).toLowerCase().endsWith("bewohn") || m.group(4).toLowerCase().endsWith("besuch")
+            || m.group(4).toLowerCase().endsWith("eit") || m.group(4).toLowerCase().endsWith("ütz")
+            || m.group(4).toLowerCase().endsWith("fahr") || m.group(4).toLowerCase().endsWith("arbeitnehm")) {
+          replace = m.group(1).substring(0,m.group(1).length()-m.group(3).length()) + m.group(4)+"er";
+        }
+        else if(m.group(4).toLowerCase().endsWith("zufußgeh")) {
+          replace = m.group(4).substring(0,m.group(4).length()-8);
+          
+          if(Character.isUpperCase(m.group(4).charAt(m.group(4).length()-8))) {
+            replace += "Fußgänger";
+          }
+          else {
+            replace += "fußgänger";
+          }
         }
         
-        System.out.println(m.group(4) + "   " + replace+"\n");
+        if(!replace.equals(m.group(1))) {
+          if(m.group(6) != null && !m.group(6).trim().isEmpty()) {
+            replace += m.group(6);
+          }
+     
+          if(m.group(5) == null && m.group(2) != null && m.group(2).toLowerCase().equals("die")) {
+            replace += "in";
+          }
+          else if(m.group(5) != null && m.group(2) != null && (m.group(2).toLowerCase().equals("mit") || m.group(2).toLowerCase().equals("bei") 
+              || m.group(2).endsWith("en")) && replace.toLowerCase().endsWith("er")) {
+            replace += "n";
+          }
+          
+         // System.out.println(m.group(1) + " " + m.group(6) + " " + replace);
+          mCountPartizip++;
+        }
+        
+      // System.out.println(m.group(4) + "   " + replace+"\n");
         result.append(text.substring(pos,m.start(1))).append(replace);
         
         pos = m.end();
       }
+      
+      
       
       if(pos < text.length()) {
         result.append(text.substring(pos,text.length()));
