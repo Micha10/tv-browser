970a45c113ca954b97e400a2343f404674aae85b
ds10 <ds10@localhost>
2022-04-09 21:19:05 +0000

    Changed version to 4.2.4.95 Beta 1.

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -203,7 +203,7 @@ public class TVBrowser {
   private static final boolean IS_STABLE = false;
   private static final int MAJRO_VERSION = 4;
   private static final int MINOR_VERSION = 24;
-  private static final int SUB_MINOR_VERSION = 51;
+  private static final int SUB_MINOR_VERSION = 95;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
@@ -217,6 +217,7 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
+      "4.2.4.95 Beta1",
       "4.2.4.51 SVN",
       "4.2.4.50 SVN",
           "4.2.4", "4.2.3.95 Beta1", "4.2.3.50 SVN",
