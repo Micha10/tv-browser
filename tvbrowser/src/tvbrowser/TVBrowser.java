fb244c4e60784b3745fdae4a7db611cba8062af1
ds10 <ds10@localhost>
2022-12-28 10:23:28 +0000

    Changed version to 4.2.7.

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -200,10 +200,10 @@ public class TVBrowser {
   
   public static final ArrayList<Image> ICONS_WINDOW = new ArrayList<Image>(4);
 
-  private static final boolean IS_STABLE = false;
+  private static final boolean IS_STABLE = true;
   private static final int MAJRO_VERSION = 4;
-  private static final int MINOR_VERSION = 26;
-  private static final int SUB_MINOR_VERSION = 95;
+  private static final int MINOR_VERSION = 27;
+  private static final int SUB_MINOR_VERSION = 0;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
@@ -217,8 +217,7 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
-      "4.2.6.95 Beta1",
-      "4.2.6.50 SVN",
+          "4.2.7", "4.2.6.95 Beta1", "4.2.6.50 SVN",
           "4.2.6", "4.2.5.50 SVN",
           "4.2.5", "4.2.4.95 Beta1", "4.2.4.51 SVN", "4.2.4.50 SVN",
           "4.2.4", "4.2.3.95 Beta1", "4.2.3.50 SVN",
